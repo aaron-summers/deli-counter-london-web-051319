b57459b21946af62204cb27106795653d1430a2a
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-09 23:19:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length >= 1
+    list = []
+    counter = 1 
+    katz_deli.each do |name|
+      list << ("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{list.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(name, katz_deli)
+  
+  
+
+end
